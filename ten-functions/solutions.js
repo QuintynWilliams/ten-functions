e046a3824ce64c69add54b52a43ab6a350fe802e
Quintyn Williams <t.quin.williams@gmail.com>
2023-02-14 14:43:23 -0600

    ten-functions solved

--- ten-functions/solutions.js
+++ ten-functions/solutions.js
@@ -21,42 +21,52 @@
  * concat
  */
 
+//EX 01
 const isTrue = (input) => {
     return input === true
 }
 
+//EX 02
 const isFalse = (input) => {
     return input === false
 }
 
+//EX 03
 const not = (input) => {
     return !input
 }
 
+//EX 04
 const addOne = (input) => {
     return parseFloat(input) + 1
 }
 
+//EX 05
 const isEven = (input) => {
     return parseFloat(input) % 2 === 0
 }
 
+//EX 06
 const isIdentical = (inpFirst, inpSecond) => {
     return inpFirst === inpSecond
 }
 
+//EX 07
 const isEqual = (inpFirst, inpSecond) => {
     return inpFirst == inpSecond
 }
 
+//EX 08
 const or = (inpFirst, inpSecond) => {
     return inpFirst || inpSecond
 }
 
+//EX 09
 const and = (inpFirst, inpSecond) => {
     return inpFirst && inpSecond
 }
 
+//EX 10
 const concat = (inpFirst, inpSecond) => {
     return `${inpFirst}` + `${inpSecond}`
 }
